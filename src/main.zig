e00bf10f2d9b195a9049df8b153374eaf52f82ec
Victor <vctr@hey.com>
2024-04-09 17:21:14 +0100

    feat & misc: messing around with pointers and added operations (unimpl)

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,7 @@ const EXCF = enum(u16) { FP = 1 << 0, FZ = 1 << 1, FN = 1 << 2 };
 
 const Vm = struct {
     const Self = @This();
+
     // Memory for the VM -> 16 bits
     // Start from beginning of memory as we are not implementing anything else in our VM.
     mem: [65535]u16 = [_]u16{0} ** 65535,
@@ -33,9 +34,6 @@ const Vm = struct {
     }
 
     // TODO: impl calling instruction with function pointers.
-    fn get_opc(instr: u16) u16 {
-        return instr >> 12;
-    }
 
     fn update_flag(self: *Self, register: REGISTER_ADDR) !void {
         if (self.registers[register] == EXCF.FP) {
@@ -46,7 +44,61 @@ const Vm = struct {
             self.registers[REGISTER_ADDR.RCND] = REGISTER_ADDR.FP;
         }
     }
+
+    // Instructions (TODO: Impl instructions)
+
+    // Branch
+    fn br(self: *Self, index: u16) !void {}
+
+    // Add
+    fn add(self: *Self, index: u16) !void {}
+
+    // Load
+    fn ld(self: *Self, index: u16) !void {}
+
+    // Store
+    fn st(self: *Self, index: u16) !void {}
+
+    // Jump to subroutine
+    fn jsr(self: *Self, index: u16) !void {}
+
+    // Bitwise AND
+    fn nd(self: *Self, index: u16) !void {}
+
+    // Load base
+    fn ldr(self: *Self, index: u16) !void {}
+
+    // Store base
+    fn str(self: *Self, index: u16) !void {}
+
+    // Return from interrupt (not impl TODO: lookup and impl)
+    fn rti(self: *Self, index: u16) !void {}
+
+    // Bitwise NOT
+    fn not(self: *Self, index: u16) !void {}
+
+    // Load indirect
+    fn ldi(self: *Self, index: u16) !void {}
+
+    // Store indirect
+    fn sti(self: *Self, index: u16) !void {}
+
+    // Jump/Return to subroutine
+    fn jmp(self: *Self, index: u16) !void {}
+
+    // Unused
+    fn u(self: *Self, index: u16) !void {}
+
+    // Load effective address
+    fn lea(self: *Self, index: u16) !void {}
+
+    // System/Trap call
+    fn trap(self: *Self, index: u16) !void {}
 };
+
+fn get_opc(instr: u16) u16 {
+    return instr >> 12;
+}
 
 // const test_struct = struct {
 //     mem: [65535]u16 = [_]u16{0} ** 65535,
@@ -66,5 +118,5 @@ pub fn main() !void {
 
     try vm.write_mem(15, 10);
 
-    std.debug.print("Hello world!\n{}\n", .{vm.read_mem(15)});
+    std.debug.print("{}\n{}\n", .{ vm.read_mem(15), get_opc(65000) });
 }
